1e3dc2e4cd9767e4ea842082d5bffbed9d55b05c
Dylan Ang <dylanmouang@gmail.com>
2022-07-30 11:11:50 -0700

    Fill pages with more content

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -1,25 +1,68 @@
 
 function About() {
     return (
-        <p>This is my About page</p>
+        <div class="about">
+            <h2 class="contentHeader">About Me</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
     )
 }
 
 function Contact() {
+    let email = {};
+    email.name = "dylanmouang"
+    email.service = "gmail"
     return (
-        <p>This is my Contact page</p>
+        <div class="contact">
+            <h2 class="contentHeader">Contact Me</h2>
+            <ul>
+                <li>{email.name}@{email.service}.com</li>
+                <li><a class="link" href="https://github.com/stack-dylan">GitHub</a></li>
+                <li><a class="link" href="https://www.linkedin.com/in/dylan-mou-ang/">LinkedIn</a></li>
+            </ul>
+        </div>
     )
 }
 
 function Experience() {
     return (
-        <p>This is my Experience page</p>
+        <div class="experience">
+            <h2 class="contentHeader">My Experience</h2>
+            <p>Thanks for Reading!</p>
+        </div>
     )
 }
 
 function Resume() {
     return (
-        <p>This is my Resume page</p>
+        <div class="resume">
+            <h2 class="contentHeader">My Resume</h2>
+            <h3>Profile</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+            <h3>Education</h3>
+            <ul>
+                <li>University of California, Davis — Computer Science, 2024</li>
+                <li>GPA — 3.77</li>
+                <li>Relevant Coursework
+                    <ul>
+                        <li>Data Structures and Algorithms</li>
+                        <li>Web Development — React, Node</li>
+                        <li>Intro to AI — Alpha Beta Pruning, PyTorch</li>
+                    </ul>
+                </li>
+            </ul>
+            
+            <h3>Projects</h3>
+            <b>Project name</b>
+            
+            <h3>Skills</h3>
+            <ul>
+                <li>Significant Experience In — Python, JavaScript</li>
+                <li>Comfortable Working in — React.js, Node.js, C/C++, SQLite3, R</li>
+                <li>I Have Used — PyTorch, Google Cloud Platform, Flask</li>
+            </ul>
+        </div>
     )
 }
 
